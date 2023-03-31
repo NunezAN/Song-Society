4746a749d17439cbda80b09fef60e927299e6346
NunezAN <alex101nunez@gmail.com>
2023-03-31 17:16:16 -0500

    initial search template

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SearchBar = () => {
+  return (
+    <div class="inputbox">
+      <input required="required" type="text" />
+      <span>Enter a Youtube Link</span>
+      <i></i>
+    </div>
+  );
+};
+
+export default SearchBar;
