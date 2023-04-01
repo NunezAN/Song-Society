4fd44a4185e067e94fe8ba17931acfab99357377
NunezAN <alex101nunez@gmail.com>
2023-03-31 19:57:04 -0500

    video is playing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import SearchBar from "./components/SearchBar";
+import ReactPlayer from "react-player";
 
 function App() {
   const [playlist, setPlaylist] = useState([]);
+  const playerRef = useRef(null);
 
+  const handleVideoEnded = () => {
+    const removed = [...playlist];
+    setPlaylist(removed.slice(1));
+  };
   return (
     <div className="bg-black min-w-full min-h-screen">
       <div className="flex flex-col w-[80%] mx-auto pb-12">
@@ -12,7 +18,24 @@ function App() {
           Song Society
         </h1>
       </div>
-      <SearchBar />
+      <SearchBar setPlaylist={setPlaylist} />
+      {playlist.length == 0 ? (
+        <h1 className="text-white text-center text-3xl p-20">
+          There are no songs in the playlist! 😞
+        </h1>
+      ) : (
+        <div className="p-16 w-full h-[600px]">
+          <ReactPlayer
+            ref={playerRef}
+            playing={true}
+            controls={true}
+            onEnded={handleVideoEnded}
+            url={playlist[0]}
+            width="100%"
+            height="100%"
+          />
+        </div>
+      )}
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useRef } from "react";
 
-const SearchBar = () => {
+const SearchBar = ({ setPlaylist }) => {
+  const inputRef = useRef("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputRef.current.value);
+    setPlaylist((cur) => [...cur, inputRef.current.value]);
+    inputRef.current.value = "";
+  };
   return (
-    <div className="inputbox">
-      <input required="required" type="text" />
+    <form className="inputbox" onSubmit={(e) => handleSubmit(e)}>
+      <input ref={inputRef} required="required" type="text" />
       <span>Enter a Youtube Link</span>
       <i></i>
-    </div>
+    </form>
   );
 };
 
